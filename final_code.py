40c1226f2110a069b5ffb4dd6ed2c39084824888
Shubham Gupta <shubham15gupta09@gmail.com>
2019-04-07 13:11:10 +0530

    Update final_code.py

--- final_code.py
+++ final_code.py
@@ -6,7 +6,8 @@ def generate(name):
     img = Image.open("CertificateTemplate.png")     #template
     draw = ImageDraw.Draw(img)
     selectFont = ImageFont.truetype("comics.ttf", size=25)   #font selection
-    draw.text((430,360), name , font=selectFont, fill=(105, 105, 105))
+    draw.text((430,360), name , font=selectFont, fill=(105, 105, 105))  
+    #430,360 is the x,y co-ordinates 105,105,105 is the code for font colour
     img.save(name + ".png")        #certificate will be saved with the name of attendee
 fin = open("new.txt",'r')   #txt file containing the names of the attendee
 for i in fin:
