bd464cfe81ec6f0ee75e7aa9db3a1192b28c6728
Shubham Gupta <shubham15gupta09@gmail.com>
2019-04-07 13:09:47 +0530

    Update final_code.py

--- final_code.py
+++ final_code.py
@@ -3,12 +3,12 @@ from PIL import ImageDraw
 from PIL import ImageFont
 
 def generate(name):
-    img = Image.open("final ashwini.png")
+    img = Image.open("CertificateTemplate.png")     #template
     draw = ImageDraw.Draw(img)
-    selectFont = ImageFont.truetype("comics.ttf", size=25)
+    selectFont = ImageFont.truetype("comics.ttf", size=25)   #font selection
     draw.text((430,360), name , font=selectFont, fill=(105, 105, 105))
-    img.save(name + ".png")
-fin = open("new.txt.txt",'r')
+    img.save(name + ".png")        #certificate will be saved with the name of attendee
+fin = open("new.txt",'r')   #txt file containing the names of the attendee
 for i in fin:
     i=i.replace("\n","")
     generate(i)
