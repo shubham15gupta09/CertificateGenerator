66180d6dddc10defaab4d52641b9a2361ec54f64
Shubham Gupta <shubham15gupta09@gmail.com>
2019-04-07 13:20:01 +0530

    Update final_code.py

--- final_code.py
+++ final_code.py
@@ -3,13 +3,14 @@ from PIL import ImageDraw
 from PIL import ImageFont
 
 def generate(name):
-    img = Image.open("CertificateTemplate.png")     #template
+    img = Image.open("Template.png")     #  template
     draw = ImageDraw.Draw(img)
-    selectFont = ImageFont.truetype("comics.ttf", size=25)   #font selection
+    selectFont = ImageFont.truetype("comics.ttf", size=25)   #  font selection
     draw.text((430,360), name , font=selectFont, fill=(105, 105, 105))  
-    #430,360 is the x,y co-ordinates 105,105,105 is the code for font colour
-    img.save(name + ".png")        #certificate will be saved with the name of attendee
-fin = open("new.txt",'r')   #txt file containing the names of the attendee
+    #  430,360 is the x,y co-ordinates 105,105,105 is the code for font colour
+    #  you need to select the x,y co-ordinate by hit and trial only
+    img.save(name + ".png")        #  certificate will be saved with the name of attendee
+fin = open("new.txt",'r')   #  txt file containing the names of the attendee
 for i in fin:
     i=i.replace("\n","")
     generate(i)
